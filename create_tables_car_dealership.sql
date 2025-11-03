f9d99043cd8bd4f9cc23141e97d1cc8f770f100f
HodaJahangiri <hoda.cs87@gmail.com>
2025-11-03 15:16:55 -0800

    Initial commit, tables are created for homework.

---
+++ create_tables_car_dealership.sql
@@ -0,0 +1,94 @@
+PRAGMA foreign_keys = ON;
+CREATE TABLE cars(
+    id INTEGER PRIMARY KEY,
+    sale_person_id INTEGER,
+    customer_id INTEGER,
+    FOREIGN KEY (sale_person_id) REFERENCES salesperson(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    vin VARCHAR(200) UNIQUE NOT NULL,
+    brand VARCHAR(200),
+    model VARCHAR(200),
+    year CHECK(year <= 2026),
+    cost DECIMAL(8,2),
+    is_new BOOLEAN,
+    mileage INTEGER CHECK(mileage >= 0)
+);
+
+CREATE TABLE invoices(
+    id INTEGER PRIMARY KEY,
+    car_id INTEGER UNIQUE,
+    sale_date DATE,
+    total_cost DECIMAL(8,2),
+    FOREIGN KEY(car_id) REFERENCES cars(id)
+);
+
+CREATE TABLE salespersons(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(350),
+    last_name VARCHAR(350),
+    phone VARCHAR(100),
+    email VARCHAR(250)
+);
+
+CREATE TABLE cars(
+    id INTEGER PRIMARY KEY,
+    salesperson_id INTEGER,
+    customer_id INTEGER,
+    vin VARCHAR(200) NOT NULL UNIQUE,
+    brand VARCHAR(100),
+    model VARCHAR(100),
+    year INTEGER CHECK (year <= 2026),
+    cost DECIMAL(8,2),
+    is_new BOOLEAN,
+    mileage INTEGER CHECK(mileage >= 0),
+    FOREIGN KEY (salesperson_id) REFERENCES salespersons(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE service_tickets(
+    id INTEGER PRIMARY KEY,
+    car_id INTEGER,
+    service_date DATE,
+    service_type VARCHAR(100) CHECK(service_type IN ('routine_maintenance', 'mechanical_repairs', 'check_services')),
+    description TEXT,
+    labor_cost DECIMAL(8,2),
+    parts_cost DECIMAL(8,2),
+    total_cost DECIMAL(8,2),
+    status VARCHAR CHECK(status IN ('pending', 'in_process', 'on_hold', 'finished')),
+    FOREIGN KEY (car_id) REFERENCES cars(id)
+);
+
+CREATE TABLE customers(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(350),
+    last_name VARCHAR(350),
+    phone VARCHAR(100),
+    email VARCHAR(250),
+    street VARCHAR(150),
+    city VARCHAR(150),
+    state VARCHAR(150),
+    zip_code INTEGER
+);
+
+CREATE TABLE mechanics(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(350),
+    last_name VARCHAR(350),
+    phone VARCHAR(100),
+    email VARCHAR(250),
+    specialization VARCHAR CHECK( specialization IN ('engine', 'transmission', 'brakes', 'electrical_systems','air_conditioning','heavy_equipment'))
+);
+
+CREATE TABLE mechanic_services(
+    id PRIMARY KEY,
+    service_ticket_id INTEGER,
+    mechanic_id INTEGER,
+    hours_worked DECIMAL(5,2),
+    mechanic_notes TEXT,
+    FOREIGN KEY (service_ticket_id) REFERENCES service_tickets(id),
+    FOREIGN KEY (mechanic_id) REFERENCES mechanics(id)
+);
+
+
+
+
---
+++ insert_values_car_dealership.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO cars(salesperson_id, customer_id, vin, brand, model, year, cost, is_new , mileage)
+-- VALUES (1, 2, '4Y1SL65848Z411439', 'Toyota', 'Camry', 2022, 19000.99, FALSE, 50000),
+-- (NULL, 3, '1FT7X2BN4RED77722', 'Lexus', 'NX300h', 2017, 20000.99, FALSE, 130000),
+-- (1, 1, 'JM1HD4618N0124832', 'Mazda', '929', 2024, 25000.99, FALSE, 30000),
+-- (2, 2, 'YV1LW5244W2522439', 'Honda', 'VT750C2', 2026, 320000.99, TRUE, 10),
+-- (3, 5, '1HFTE44H7F4102747', 'Mitsubishi', '3000GT', 1995, 2000.99, FALSE, 330000),
+-- (NULL, 2, '1FTHS2426VHA63343', 'Chevrolet', 'C/K1500', 1989, 25000.99, FALSE, 10000),
+-- (4, 4, 'JF1GM2253TG415807', 'Subaru', 'Impreza', 1996, 7000.99, FALSE, 300000),
+-- (5, 1, '1F65F5KY3B0A01654', 'Ford', 'Stripped', 2011, 8000.99, FALSE, 220000);
+
+
+
+
